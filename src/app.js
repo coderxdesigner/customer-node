271ee71a3b04ac4e4b1ed95d570e05d1fb3ec73b
Rachel H. <rachel.heneault@gmail.com>
2024-04-08 20:03:07 -0400

    Creating a hardcoded api / data

--- src/app.js
+++ src/app.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
-
+const customers = [
+    {   "name": "Rachel",
+        "industry":"web development"
+    },
+    {
+        "name":"Sarah",
+        "industry":"game designer"
+    },
+    {
+        "name":"Caleb",
+        "industry":"music"
+    }
+    
+]
 
 app.get('/', (req,res)=>{
-    res.send('<h1>Rachel H</h1>')
+    res.send({"customers":customers})
 })
 app.listen(PORT, ()=> {
     console.log('app listening on port')
