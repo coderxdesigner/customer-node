7121656406d5e382e734e513925c3d0a75a7825a
Rachel H. <rachel.heneault@gmail.com>
2024-04-10 20:25:33 -0400

    added data and a post req

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
-const PORT = 3000
+const PORT = 3000;
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 const customers = [
     {   "name": "Rachel",
         "industry":"web development"
@@ -21,4 +24,9 @@ app.get('/', (req,res)=>{
 })
 app.listen(PORT, ()=> {
     console.log('app listening on port')
+})
+
+app.post('/api/customers', (req,res)=> {
+    console.log(req.body);
+    res.send(req.body);
 })
